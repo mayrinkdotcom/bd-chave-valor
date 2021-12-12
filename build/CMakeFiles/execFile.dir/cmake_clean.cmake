b5c9e27dcd232e0796747604a47a9efb468461ea
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-12-12 18:27:30 -0300

    implementando CMake para build automatizado

---
+++ build/CMakeFiles/execFile.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/execFile.dir/src/main.c.o"
+  "execFile"
+  "execFile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/execFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
